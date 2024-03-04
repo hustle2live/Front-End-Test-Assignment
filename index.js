51436ea28fd864bff4d78a6359dae9cacc1306f4
volodymyr kuznietsov <kuz.v@ukr.net>
2024-03-04 23:45:10 +0200

    moderate styles

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ document.querySelector('.radio-select .btn-custom').addEventListener('click', (e
    e.currentTarget.classList.toggle('active');
    document.querySelector('.radio-select .size_select').classList.toggle('custom');
 });
+
+const tooltipWrappersAll = document.querySelectorAll('.tooltip-wrapper i');
+
+tooltipWrappersAll.forEach((i) =>
+   i.addEventListener('click', (e) => {
+      e.currentTarget.nextElementSibling.classList.toggle('hidden');
+   })
+);
